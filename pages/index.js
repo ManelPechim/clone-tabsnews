1ea1b68a6a44387724b30b167440fbc1e41bae4d
ManelPechim <ferreiraemanuel798@gmail.com>
2026-04-29 15:57:19 -0300

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1>Não tenho mulher para mandar uma mensagem fofa e nem que impacte, então essa vai pro meu cachorro Boris! Te amo meu gordinho sz</h1>
+};
+
+export default Home;
