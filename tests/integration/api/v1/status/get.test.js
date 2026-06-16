f2d44e85072701f000b082f3c4bf4f1d9ceaa841
ManelPechim <ferreiraemanuel798@gmail.com>
2026-06-16 01:29:04 -0300

    finaliza o endpoint `api/v1/status`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,17 @@
 test("GET do /api/v1/status deve retornar 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  const database = responseBody.dependencies.database;
+
+  // expect(responseBody.updated_at).toBeDefined();
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(database.version).toEqual("16.0");
+  expect(database.max_connections).toEqual(100);
+  expect(database.opened_connections).toEqual(1);
+  // expect(database.opened_connections).toBeGreaterThan(0);
+  // expect(database.opened_connections).toBeLessThanOrEqual(database.max_connections);
 });
