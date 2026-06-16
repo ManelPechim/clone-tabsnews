2e8dd8361fdce3c75943d754cd06a11c94319552
ManelPechim <ferreiraemanuel798@gmail.com>
2026-06-16 01:27:45 -0300

    faz o `database.js` ser mais robusto para tratamentos de erros

--- infra/database.js
+++ infra/database.js
@@ -9,9 +9,21 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD
   });
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+  // client.connect((err) => {
+  //   if (err) throw new Error('Algo deu errado: ', err);
+  //   client.query(queryObject, () => {
+  //     client.end()
+  //   })
+
+  // });
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 };
 
 export default {
