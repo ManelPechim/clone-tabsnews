54f6be3d61c0d1296882e59ea65a0db646e7eca5
ManelPechim <ferreiraemanuel798@gmail.com>
2026-06-20 20:13:51 -0300

    adiciona endpoint `/api/v1/migrations`

--- infra/database.js
+++ infra/database.js
@@ -1,23 +1,15 @@
 import { Client } from "pg";
+// client.connect((err) => {
+//   if (err) throw new Error('Algo deu errado: ', err);
+//   client.query(queryObject, () => {
+//     client.end()
+//   })
 
+// });
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: getSSLValues(),
-  });
-  // client.connect((err) => {
-  //   if (err) throw new Error('Algo deu errado: ', err);
-  //   client.query(queryObject, () => {
-  //     client.end()
-  //   })
-
-  // });
+  let client;
   try {
-    await client.connect();
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
@@ -26,10 +18,25 @@ async function query(queryObject) {
   } finally {
     await client.end();
   }
-};
+}
+
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  });
+
+  await client.connect();
+  return client;
+}
 
 export default {
-  query: query
+  query,
+  getNewClient,
 };
 
 function getSSLValues() {
@@ -37,6 +44,6 @@ function getSSLValues() {
     return {
       ca: process.env.POSTGRES_CA,
     };
-  };
-  return process.env.NODE_ENV === "development" ? false : true;
-};
+  }
+  return process.env.NODE_ENV === "production" ? true : false;
+}
