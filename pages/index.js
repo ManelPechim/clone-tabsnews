e18feac8987bcd0edff8f57ec3439d2c704a19c3
ManelPechim <ferreiraemanuel798@gmail.com>
2026-07-05 01:30:10 -0300

    fix linting do `eslint`

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ function Home() {
       brasileira de RPG — onde jogadores e mestres publicam, votam e debatem
       mecânicas, aventuras, relatos reais de mesa, notícias do ecossistema
       (novos sistemas, patches, lançamentos) e tudo do universo RPG, incluindo
-      jogos eletrônicos com DNA de mesa como Baldur's Gate e Divinity, com
+      jogos eletrônicos com DNA de mesa como Baldur&apos;s Gate e Divinity, com
       postura exata e empática.
     </h1>
   );
